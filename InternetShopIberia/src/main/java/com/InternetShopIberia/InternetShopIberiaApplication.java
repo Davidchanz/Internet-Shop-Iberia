acb29154f5e5aed05b830f4bb70e5f9019fe3778
Davidchanz <tovliskatsi@gmail.com>
2023-03-16 20:38:26 +0400

    Initial commit

---
+++ InternetShopIberia/src/main/java/com/InternetShopIberia/InternetShopIberiaApplication.java
@@ -0,0 +1,13 @@
+package com.InternetShopIberia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetShopIberiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetShopIberiaApplication.class, args);
+	}
+
+}
