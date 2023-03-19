bb8bd12d6e88d82a1db23ed582bf49b193b3c46e
Davidchanz <tovliskatsi@gmail.com>
2023-03-19 17:04:18 +0400

    Category
    Category abstraction - name, List(subCategories).
    StartPageController - start page controller(@GetMapping("/")).
    Start page - "/", need authority.
    On start page - Right side menu with tree structured categories collection.

--- src/main/java/com/InternetShopIberia/controller/StartPageController.java
+++ src/main/java/com/InternetShopIberia/controller/StartPageController.java
@@ -1,14 +1,14 @@
-package com.InternetShopIberia;
+package com.InternetShopIberia.controller;
 
+import com.InternetShopIberia.Category;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-public class testController {
+public class StartPageController {
     @GetMapping("/")
     public String contacts(Model model) {
         Category categories = new Category("Categories", List.of(
