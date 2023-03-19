f2bc565cee451b147cfe6405e094d9cc7fa3aaf8
Davidchanz <tovliskatsi@gmail.com>
2023-03-19 17:17:20 +0400

    Create category
    Moved Category class into model package.

--- src/main/java/com/InternetShopIberia/controller/StartPageController.java
+++ src/main/java/com/InternetShopIberia/controller/StartPageController.java
@@ -1,6 +1,6 @@
 package com.InternetShopIberia.controller;
 
-import com.InternetShopIberia.Category;
+import com.InternetShopIberia.model.Category;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
