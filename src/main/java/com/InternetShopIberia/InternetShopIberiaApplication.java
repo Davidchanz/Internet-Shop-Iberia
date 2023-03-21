257f617c2018c263ca954eaa387e23756e0e9e52
Davidchanz <tovliskatsi@gmail.com>
2023-03-21 18:05:50 +0400

    Add price format

--- src/main/java/com/InternetShopIberia/InternetShopIberiaApplication.java
+++ src/main/java/com/InternetShopIberia/InternetShopIberiaApplication.java
@@ -26,14 +26,17 @@ public class InternetShopIberiaApplication {
 		var categoryService = context.getBean(CategoryService.class);
 		var category = categoryService.findCategoryById(1L);
 
-		Product product = new Product();
-		product.setName("Acer Predator Helios 300 PH315-54-760S Gaming Laptop");
-		product.setCategory(category);
-		product.setDescription("Acer Predator Helios 300 PH315-54-760S Gaming Laptop | Intel i7-11800H | NVIDIA GeForce RTX 3060 GPU | 15.6\" FHD 144Hz 3ms IPS Display | 16GB DDR4 | 512GB SSD | Killer WiFi 6 | RGB Keyboard");
-		product.setPrice(new BigDecimal("2199.00"));
-		product.setPId(4804708L);
+		for (int i = 0; i < 10; i++) {
 
-		productService.addProduct(product);
+			Product product = new Product();
+			product.setName("Acer Predator Helios 300 PH315-54-760S Gaming Laptop");
+			product.setCategory(category);
+			product.setDescription("Acer Predator Helios 300 PH315-54-760S Gaming Laptop | Intel i7-11800H | NVIDIA GeForce RTX 3060 GPU | 15.6\" FHD 144Hz 3ms IPS Display | 16GB DDR4 | 512GB SSD | Killer WiFi 6 | RGB Keyboard");
+			product.setPrice(new BigDecimal("2199.00"));
+			product.setPId(i+4804708L);
+
+			productService.addProduct(product);
+		}
 	}
 
 	private static void addCategoryTemplate(ConfigurableApplicationContext context){
