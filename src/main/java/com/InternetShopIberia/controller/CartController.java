ca6b7184fc0d38fd51701438a2c4e8eb313e9aa1
Davidchanz <tovliskatsi@gmail.com>
2023-04-06 13:45:58 +0400

    Change CartController Scope on Session

--- src/main/java/com/InternetShopIberia/controller/CartController.java
+++ src/main/java/com/InternetShopIberia/controller/CartController.java
@@ -6,6 +6,7 @@ import com.InternetShopIberia.model.Product;
 import com.InternetShopIberia.model.User;
 import com.InternetShopIberia.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -19,6 +20,7 @@ import java.util.List;
 import java.util.Objects;
 
 @Controller
+@Scope("session")
 public class CartController {
     @Autowired
     private CartService cartService;
