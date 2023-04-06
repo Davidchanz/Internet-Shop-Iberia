709005b68396413ed4a4963a9549333c69b01387
Davidchanz <tovliskatsi@gmail.com>
2023-04-06 13:48:00 +0400

    Save searchRequest in session attribute.

--- src/main/java/com/InternetShopIberia/controller/SearchResultsController.java
+++ src/main/java/com/InternetShopIberia/controller/SearchResultsController.java
@@ -8,15 +8,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class SearchResultsController {
     @Autowired
     private ProductService productService;
 
     @GetMapping("/search")
-    private String showSearchResult(@RequestParam("searchRequest") String searchRequest, Model model){
+    private String showSearchResult(@RequestParam("searchRequest") String searchRequest, HttpSession session, Model model){
         var products = productService.getAllProductsByName(searchRequest);
         model.addAttribute("products", products);
+        session.setAttribute("searchRequest", searchRequest);
         return "products";
     }
 }
