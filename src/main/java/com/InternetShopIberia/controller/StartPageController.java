11e10b21ee2093c6d4ca462a84a546025b4ddaab
Davidchanz <tovliskatsi@gmail.com>
2023-04-29 13:32:27 +0400

    Fix product carousel resizing
    Set body min width.
    Set resize product carousel when change fullscreen or resize window.

--- src/main/java/com/InternetShopIberia/controller/StartPageController.java
+++ src/main/java/com/InternetShopIberia/controller/StartPageController.java
@@ -10,17 +10,6 @@ import javax.servlet.http.HttpSession;
 public class StartPageController {
     @GetMapping("/")
     public String showStartPage(HttpSession session, Model model) {
-
-        //Error handle and show
-        /*try{}
-        catch(RuntimeException e){
-            Error error = new Error();
-            error.setCode(102L);
-            error.setMessage("Test error!");
-            model.addAttribute("error", error);
-            return "startPage";
-        }*/
-
         return "startPage";
     }
 }
