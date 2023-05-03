2476ea17436fbb6040fbc38a0e186b175d8ba347
Davidchanz <tovliskatsi@gmail.com>
2023-05-03 18:10:31 +0400

    Added custom error pages for 404 and 500 errors.
    Configuration - WebConfig - method customize() -
       added errorPages 404, 500.
    Controller - CustomErrorController - added method
       notFound - return "404.html" error page.
       internal - return "500.html" error page.
    "404.html" - error page for error 404 not found.
    "500.html" - error page for error 500 not found.
    
    Delete
    
    "error.html".
    ErrorController.

---
+++ src/main/java/com/InternetShopIberia/configuration/WebConfig.java
@@ -0,0 +1,18 @@
+package com.InternetShopIberia.configuration;
+
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+
+@Configuration
+public class WebConfig implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+
+    @Override
+    public void customize(ConfigurableServletWebServerFactory factory) {
+        factory.addErrorPages(
+                new ErrorPage(HttpStatus.NOT_FOUND, "/404"),
+                new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500"));
+    }
+}
